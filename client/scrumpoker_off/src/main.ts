62961bece9073c2a5973a4eb0b749c986702ce3f
Sebastian Weszler <s.weszler@gmail.com>
2021-02-11 13:46:09 +0800

    remove old client app

--- client/scrumpoker_off/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import config from './config.js'
-
-const app = createApp(App);
-app.config.globalProperties.$api = config.API_URL;
-app.mount('#app');
