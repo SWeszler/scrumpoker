10724c5f855b204bee1439ba1642744e9025d74b
Sebastian Weszler <s.weszler@gmail.com>
2021-02-11 16:11:03 +0800

    add websocket connection for the room

--- client/scrumpoker/src/views/Room.vue
+++ client/scrumpoker/src/views/Room.vue
@@ -7,12 +7,20 @@
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs } from 'vue';
+const WS_URL = process.env.VUE_APP_WS_URL;
 
 export default defineComponent({
   setup(){
     const compData = reactive({
       activeUsers: ["test"]
     });
+
+    const ws = new WebSocket(WS_URL + 'ws/join-game/');
+    ws.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      console.log(data);
+      compData.activeUsers = data.players;
+    }
 
     return { ...toRefs(compData) };
   }
